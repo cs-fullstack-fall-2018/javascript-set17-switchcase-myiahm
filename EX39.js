8df046734c515ddf77d5168b6d9569f5f876d07d
Myiah Moore <myiahsade@gmail.com>
2018-08-03 13:15:06 -0500

    check in

---
+++ EX39.js
@@ -0,0 +1,26 @@
+var userInput = prompt("Enter a number to print: ");
+main();
+function main () {
+
+
+    switch (userInput) {
+        case 1 :
+            console.log(1);
+            break;
+        case 2 :
+            console.log(2);
+            break;
+
+        case 3 :
+            console.log(3);
+            break;
+
+        case 4:
+            console.log(4);
+            break;
+
+        case 5:
+            console.log(5);
+            break;
+    }
+}
